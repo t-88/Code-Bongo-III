8b69631f1be2091c98d6a2ec2103a1b30572ef0d
t-88 <temptaki5002@gmail.com>
2023-06-03 08:37:20 +0100

    tokenizing the code

---
+++ mking-lang/README.md
@@ -0,0 +1,3 @@
+# Programming Lang From Scratch 
+-   fellowing this [series](https://www.youtube.com/watch?v=8VB5TY1sIRo&list=PL_2VhOvlMk4UHGqYCLWc6GO8FaPl8fQTh&ab_channel=tylerlaceby) about making a programming lang using typescrpit
+-   trying to learn parsing and lexers??
---
+++ mking-lang/lexer.ts
@@ -0,0 +1,74 @@
+// let x = 45
+// [LetToken , IdentifierToken, EqualToken, NumberTokenf]
+
+
+export enum TokenType {
+    Let,
+    Identifier,
+    Equal,
+    Number,
+    BinaryOp,
+    Oparen,
+    Cparen,
+}
+export interface Token {
+    value : string;
+    type : TokenType;
+}
+
+const KEYWORDS = {
+    "let" : TokenType.Let,
+};  
+
+export function isAlpha(src : string) : boolean {
+    return ("a" <= src && src <= "z") || ("A" <= src && src <= "Z");
+}
+export function isNumeric(src : string) : boolean {
+    return  "0" <= src && src <= "9";  
+}
+function isSkipable(src: string) : boolean {
+    return (src == " " || src == "\n" || src == "\t");
+} 
+function token(type : TokenType,value : string): Token {
+    return {type  , value} as Token;
+} 
+
+
+export function tokenize(source : string) : Token[] {
+    const tokens = new Array<Token>();
+    const src = source.split("");
+    
+    
+    while(src.length > 0) {
+        let c : string = src.shift() as string;  
+        if(c == "+" || c == "-" || c == "*" || c == "/") {
+            tokens.push(token(TokenType.BinaryOp,c))
+        } else if(c == "(") {
+            tokens.push(token(TokenType.Oparen,c))
+        } else if(c == ")") {
+            tokens.push(token(TokenType.Cparen,c))
+        } else if(c == "=") {
+            tokens.push(token(TokenType.Equal,c))
+        } else if(isNumeric(c)) {
+            let str : string = c;
+            while(src.length > 0 && isNumeric(src[0]))
+                str += src.shift();
+            tokens.push(token(TokenType.Number,str))
+        } else if(isAlpha(c)) {
+            let str : string = c;
+            while(src.length > 0 && isAlpha(src[0]))
+                str += src.shift();
+            
+            let type : TokenType = TokenType.Identifier;
+            if(KEYWORDS[str] != null) {
+                type = KEYWORDS[str];
+            }
+            tokens.push(token(type,str))
+        } else {
+            console.log("unkown char",c);
+        }
+    }
+    return tokens;
+}
+
+console.log(tokenize("let x = (15 + 3) \n asd"));
